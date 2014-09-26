2014c791a0617ef686a3713ede07a01c4d55766c
jadman02 <jadman02@hotmail.com>
2014-09-26 22:14:08 +1000

    d

---
+++ js/my-app.js
@@ -0,0 +1,362 @@
+// Initialize your app
+var myApp = new Framework7({
+  init: false //Disable App's automatica initialization
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+	
+	//Now we add our callback for initial page
+myApp.onPageInit('index', function (page) {
+  //Do something here with home page
+
+
+askLocation();
+functionEmpty();
+
+//likebutton
+
+$$('#likebutton').on('click', function (e) {
+
+var top = $$('#likebutton').scrollTop();
+
+
+
+    alert('top:' + top);
+});
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+	
+myApp.onPageInit('location', function (page) {
+
+alert('on location page');
+
+document.getElementsByName('numPeople')[0].placeholder=localStorage.getItem("position");
+
+var radius = localStorage.getItem('radius');
+if (!radius) {radius == 25}
+
+document.getElementById("amount").innerHTML = radius;
+
+//Autocomplete JSON Google
+$$('#numPeople').keyup(function(){
+
+$$.getJSON('https://maps.googleapis.com/maps/api/place/autocomplete/json?input='+ this.value +'&types=(cities)&key=AIzaSyAssayN33K28DkBxPB8iWOM0NG2-sCNHEk', function(response){
+$$("#resulty li").remove();
+for (i = 0; i < 5; i++) 
+{ 
+	
+
+	
+$$( '#resulty' ).append('<li class="item-content"><div class="item-media"></div><div class="item-inner"><div class="item-title button" style="margin-left:15px;" onclick="savePosition(\''+ response.predictions[i].place_id  +'\')">' + response.predictions[i].description + '</div></div></li>');
+}
+});    
+    
+});
+
+//Get Latitude and Longitude onclick
+
+$$('.item-title').on('click', function (e) {
+    alert('clicked');
+});
+
+
+
+
+	
+});	
+
+
+
+ 
+//And now we initialize app
+myApp.init();
+
+
+
+
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+	
+myApp.onPageInit('register', function (page) {
+
+alert('on register page');
+mainView.hideNavbar();
+
+});
+
+
+
+var register = localStorage.getItem('register');
+if (register) {
+
+if(latitude) {}
+else{mainView.loadPage('location.html');}
+	
+	
+
+
+
+	
+}
+else {
+
+// Load page from about.html file to main View:
+mainView.loadPage('register.html');
+	
+}	
+
+
+
+
+
+
+
+
+
+
+// Callbacks to run specific code for specific pages, for example for About page:
+
+myApp.onPageInit('about', function (page) {
+
+alert('on about page');
+
+	$$('.create-page').on('click', function () {
+        createContentPage();
+    });
+});
+
+
+
+
+
+function functionEmpty() {
+$$(".load_more").removeAttr("disabled", "disabled");
+$$(".load_previous").attr("disabled", "disabled");
+var track_click = 0;
+
+$$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', function(response){
+
+// Store
+localStorage.setItem("total_pages", response.length);
+$$("#loader-container").hide();
+for (i = 0; i < 5; i++) {        
+$$( '#result' ).append('<li class="swipeout"><div class="swipeout-content"><a href="#" class="item-content item-link"><div class="item-inner"><div class="item-title-row"><div class="item-title">Facebook</div><div class="item-after">17:14</div></div><div class="item-subtitle">New messages from John Doe</div><div class="item-text">Lorem ipsum ...</div></div></a></div><div class="swipeout-actions-left"><a href="#" class="swipeout-overswipe bg-green reply">Reply</a><a href="#" class="bg-blue forward">Forward</a></div><div class="swipeout-actions-right"><a href="#" class="mark bg-orange">Mark</a><a href="#" class="swipeout-delete swipeout-overswipe">Delete</a></div></li>');
+
+}
+track_click++;
+$$(".load_more").show();
+$$(".load_previous").show();
+
+});
+
+
+$$(".load_previous").click(function (e) {
+
+$$(".load_more").removeAttr("disabled", "disabled");
+
+track_click--;
+
+$$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', function(response){
+$$("#result li").remove();
+
+
+var start = track_click * 5;
+var finish = start + 5;
+
+for (i = start; i < finish; i++) {        
+$$( '#result' ).append('<li><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><a href="#" class="item-link item-content"><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a><div class="item-input"><input type="text" placeholder="Write a comment" style="width:70%"></div><a href="#" class="button" style="width:30%;border-radius:50%;"><i class="pe-7s-like2 pe-2x"></i></a></li>');
+
+}
+
+
+
+
+
+
+
+
+});
+
+
+
+
+	
+	
+});
+
+$$(".load_more").click(function (e) {
+
+var number_pages = localStorage.getItem("total_pages");
+var stop = Math.ceil(number_pages / 5);
+
+if (track_click >= (stop-1)) {
+$$(".load_more").attr("disabled", "disabled");
+}
+
+
+
+$$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', function(response){
+$$("#result li").remove();
+
+
+var start = track_click * 5;
+var finish = start + 5;
+
+for (i = start; i < finish; i++) {        
+$$( '#result' ).append('<li><a href="#" class="item-link item-content"><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a><div class="item-input"><input type="text" placeholder="Write a comment" style="width:70%"></div><a href="#" class="button" style="width:30%;border-radius:50%;"><i class="pe-7s-like2 pe-2x"></i></a></li>');
+
+}
+
+$$(".load_previous").removeAttr("disabled", "disabled");
+
+track_click++;
+
+
+});	
+	
+});
+
+
+
+
+}
+
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+	return;
+}
+
+
+
+
+
+
+
+
+
+function askLocation() {
+	
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        //var element = document.getElementById('geolocation');
+        //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            //'Longitude: '          + position.coords.longitude             + '<br />' +
+                            //'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            //'Timestamp: '          +                                   position.timestamp          + '<br />';
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+    }
+
+function savePosition(place_id) {
+
+
+$$.getJSON('https://maps.googleapis.com/maps/api/place/details/json?placeid='+ place_id +'&key=AIzaSyAssayN33K28DkBxPB8iWOM0NG2-sCNHEk', function(response){
+
+$$("#resulty li").remove();
+
+localStorage.setItem("latitude", response.result.geometry.location.lat);
+localStorage.setItem("longitude", response.result.geometry.location.lng);
+localStorage.setItem("position", response.result.formatted_address);
+
+var elem = document.getElementById("numPeople");
+elem.value = response.result.formatted_address;
+
+});    	
+	
+	 
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function focusLocation(){
+	
+var elem = document.getElementById("numPeople");
+elem.value = "";
+}
+
+function searchButton(){
+var radius = document.getElementById("amount").value;
+var search = document.getElementById("search").value;
+var category = document.getElementById("category").value;
+var order = document.getElementById("order").value;
+
+
+localStorage.setItem("radius", radius);	
+localStorage.setItem("search", search);	
+localStorage.setItem("category", category);	
+localStorage.setItem("order", order);	
+
+}
