e730887fb7c8586ec92f548d6f3d64f28525b0b1
jadman02 <jadman02@hotmail.com>
2014-09-29 09:34:43 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -377,10 +377,7 @@ alert(post_id);
 
 $$.getJSON('http://www.smilesavers.net.au/getdeal.php?callback=?', \'post_id='+ post_id +'\', function(response){
 
-for (i = 0; i < 5; i++) {        
-alert('it worked - response:' + response[i][1] + response[i][3] + response[i][2]);
-
-}
+alert('it worked - response:');
 
 
 
