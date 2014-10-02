33c1e57a91482efeb31c9f643ed4cbda5394afa6
jadman02 <jadman02@hotmail.com>
2014-10-02 18:18:02 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -364,8 +364,8 @@ mainView.loadContent(
         '    <!-- Scrollable page content-->' +
         '    <div class="page-content" style="margin-top:30px;background-image: url(\''+ cover  +'\');background-size: 100%;background-repeat: no-repeat;">' +
         
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner" style="margin-top:30px;background-color:rgba(255,255,255,.4);">' +
+        '      <div class="content-block" style="margin-top:30px;">' +
+        '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);">' +
                       '<p>'+ title +'</p>'+
         '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
         '          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
