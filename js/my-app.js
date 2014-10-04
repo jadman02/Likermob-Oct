823dc7a0fa1ee3d8a7e4dd6cad0b09da73a854f2
jadman02 <jadman02@hotmail.com>
2014-10-04 18:23:15 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -365,7 +365,7 @@ var data = localStorage.getItem("allEntries");
 	for (i = 0; i < json.length; i++) {        
 //if (json[i].timestamp < nd){alert('deal is still valid')};
 //if (json[i].timestamp > nd){alert('deal is invalid - need to remove it'); localStorage.removeItem('entry');};
-if (json[i].post_id == post_id){alert('you like this deal');}
+//if (json[i].post_id == post_id){alert('you like this deal');}
 
 
 }
