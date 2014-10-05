03970dfd3ab78e0a2d6e8164c4c7897378ac84f3
jadman02 <jadman02@hotmail.com>
2014-10-06 10:18:09 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -380,8 +380,70 @@ var data = localStorage.getItem("allEntries");
 //if (json[i].timestamp < nd){alert('deal is still valid')};
 //if (json[i].timestamp > nd){alert('deal is invalid - need to remove it'); localStorage.removeItem('entry');};
 if (json[i].post_id == post_id){
+
+	mainView.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="right" onclick="getBusiness(\''+ page_id  +'\')"><img src="http://graph.facebook.com/'+ page_id +'/picture?width=20&height=20" style="border-radius:50%;margin-right:10px;"/>' + name + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="deal" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content" style="margin-top:30px;background-image: url(\''+ cover  +'\');background-size: 100%;background-repeat: no-repeat;">' +
+        
+        '      <div class="content-block" style="padding-top:40px;">' +
+        '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);">' +
+                      '<a href="#" class="button no-like-button" onclick="likeButton(\''+ post_id  +'\')"><i class="pe-7s-like2 pe-2x"></i></a>' + 
+                      '<p>You Like this page'+ title +'</p>'+
+                      '<p>'+ description +'</p>'+
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+
+	return;
+	
+	
 }
 else{
+
+mainView.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="right" onclick="getBusiness(\''+ page_id  +'\')"><img src="http://graph.facebook.com/'+ page_id +'/picture?width=20&height=20" style="border-radius:50%;margin-right:10px;"/>' + name + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="deal" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content" style="margin-top:30px;background-image: url(\''+ cover  +'\');background-size: 100%;background-repeat: no-repeat;">' +
+        
+        '      <div class="content-block" style="padding-top:40px;">' +
+        '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);">' +
+                      '<a href="#" class="button no-like-button" onclick="likeButton(\''+ post_id  +'\')"><i class="pe-7s-like2 pe-2x"></i></a>' + 
+                      '<p>You dont like this page'+ title +'</p>'+
+                      '<p>'+ description +'</p>'+
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+
+	return;
 	
 }
 
@@ -411,50 +473,7 @@ else{
 
 
 
-mainView.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="right" onclick="getBusiness(\''+ page_id  +'\')"><img src="http://graph.facebook.com/'+ page_id +'/picture?width=20&height=20" style="border-radius:50%;margin-right:10px;"/>' + name + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="deal" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content" style="margin-top:30px;background-image: url(\''+ cover  +'\');background-size: 100%;background-repeat: no-repeat;">' +
-        
-        '      <div class="content-block" style="padding-top:40px;">' +
-        '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);">' +
-                      '<a href="#" class="button no-like-button" onclick="likeButton(\''+ post_id  +'\')"><i class="pe-7s-like2 pe-2x"></i></a>' + 
-                      '<p>'+ title +'</p>'+
-                      '<p>'+ description +'</p>'+
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	return;
 
 
 
