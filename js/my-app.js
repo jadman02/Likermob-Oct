1308f942794c2da810e61c14ed23625091093625
jadman02 <jadman02@hotmail.com>
2014-10-06 23:17:05 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -539,7 +539,7 @@ function initialize() {
 	
   var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
   var mapOptions = {
-    zoom: 4,
+    zoom: 12,
     center: myLatlng
   }
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
