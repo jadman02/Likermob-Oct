2d74d683744adabf4d8f30a3ab2ebea73eab0858
jadman02 <jadman02@hotmail.com>
2014-10-06 18:26:40 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -365,7 +365,7 @@ function addEntry(post_id) {
 
 function getDeal(post_id,name,title,page_id,cover,singlequote) {
 
-var description = singlequote.replace(/'/g, "qqqq");
+var description = singlequote.replace(/qqqq/g, "'");
 var myDate = new Date('October 5, 2014');
 
 
