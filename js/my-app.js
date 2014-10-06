9568c65c169a9cc396d6b72d4cf638c493cb6162
jadman02 <jadman02@hotmail.com>
2014-10-06 23:12:58 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -501,7 +501,7 @@ mainView.loadContent(
         '  <div data-page="business" class="page">' +
         '    <!-- Scrollable page content-->' +
         '<div id="map-canvas"></div>' +
-        '    <div class="page-content" style=" z-index: 1;background-color:transparent;margin-top:80px;>' +
+        '    <div class="page-content" style="z-index: 1;background-color:transparent;margin-top:80px;">' +
         '      <div class="content-block">' +
         '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);"">' +
         '          <p>yoyoyoyoy<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>Here is a dynamic page created on ' + new Date() + ' !</p>' +
