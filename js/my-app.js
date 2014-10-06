50c1162f6f71f19e574f0387337d294c354ccb2e
jadman02 <jadman02@hotmail.com>
2014-10-07 09:37:01 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -509,6 +509,7 @@ $$.getJSON('http://www.smilesavers.net.au/getbusiness.php?callback=?', 'page_id=
 $$.getJSON('https://graph.facebook.com/363053000439943?fields=cover', function(response){
 	alert('got the cover photo');
 	alert(response);
+	alert(response[0][2]);
 });
 
 
