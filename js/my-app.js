e86c042362951648e22913cf7db099ba22712322
jadman02 <jadman02@hotmail.com>
2014-10-07 09:57:24 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -519,7 +519,7 @@ mainView.loadContent(
         '<div class="navbar">' +
         '  <div class="navbar-inner">' +
         '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="right"><a href="#" class="link" onclick="openMap();"><i class="pe-7s-map-marker pe-lg" style="color:#ff8000"></i> Map</a>' +
+        '    <div class="right"><div class="item-input" onclick="openMap();"><label class="label-switch"><input type="checkbox"><div class="checkbox"></div></label></div>' +
         '  </div>' +
         '</div>' +
         '<div class="pages">' +
