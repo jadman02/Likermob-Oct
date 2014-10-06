8e87379606c56f0424e5d60302680ed98bdc3e6d
jadman02 <jadman02@hotmail.com>
2014-10-07 09:46:19 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -508,8 +508,8 @@ $$.getJSON('http://www.smilesavers.net.au/getbusiness.php?callback=?', 'page_id=
 
 $$.getJSON('https://graph.facebook.com/363053000439943?fields=cover', function(response){
 	
-	var res= JSON.parse(response);
-               alert(res[0].cover["source"]);
+	var json = JSON.parse(response);
+	alert(json.cover.source);
 });
 
 
