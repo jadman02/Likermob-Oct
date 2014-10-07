a88617c8f65a0bad7da885d571e9784bff7312dc
jadman02 <jadman02@hotmail.com>
2014-10-07 12:47:13 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -538,9 +538,10 @@ mainView.loadContent(
         '    <div class="page-content" style="z-index: 1;margin-top:30px;">' +
         '      <div class="content-block" style="padding-top:40px;">' +
         '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);"">' +
-    '<div class="content-block-title">Current Deals</div><div class="slider-container slider-2 slider-init"><div class="slider-pagination"></div><div class="slider-wrapper"><div class="slider-slide"><span>Slide 1</span></div><div class="slider-slide"><span>Slide 2</span></div><div class="slider-slide"><span>Slide 3</span></div><div class="slider-slide"><span>Slide 4</span></div><div class="slider-slide"><span>Slide 10</span></div></div></div>'+
+    
         '<img src="http://graph.facebook.com/'+ page_id +'/picture?width=20&height=20" style="border-radius:50%;margin-right:10px;"/><h1>'+ name +'</h1>' +
-        '          <p>yoyoyoyoy<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>2/30 First Street Clayton South</p>' +
+        '<div class="content-block-title">Current Deals</div><div class="slider-container slider-2 slider-init"><div class="slider-pagination"></div><div class="slider-wrapper"><div class="slider-slide"><span>Slide 1</span></div><div class="slider-slide"><span>Slide 2</span></div><div class="slider-slide"><span>Slide 3</span></div><div class="slider-slide"><span>Slide 4</span></div><div class="slider-slide"><span>Slide 5</span></div></div></div>'+
         '        </div>' +
         '      </div>' +
         '    </div>' +
