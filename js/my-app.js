2848aa77726d28c9bbdc50afa684f8b0aede7612
jadman02 <jadman02@hotmail.com>
2014-10-09 09:31:35 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -90,7 +90,7 @@ ptrContent.on('refresh', function (e) {
     // Emulate 2s loading
     setTimeout(function () {
 
-    alert('pulled');
+    functionEmpty();
 
         // When loading done, we need to reset it
         myApp.pullToRefreshDone();
