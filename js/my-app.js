922b7e7981b39de0fd4104a5fe4034193927a82e
jadman02 <jadman02@hotmail.com>
2014-10-09 16:22:23 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -642,7 +642,7 @@ function popUp(title,description,page_id){
 myApp.modal({
     title: '<span style="padding-left:15px;padding-right:15px;">' + title + '</span>',
     text: '<span style="padding-left:15px;padding-right:15px;">' + description + '</span>',
-    afterText: '<div style="border-top:1px solid #ccc;width:100%"><input id="commentbox" type="text" placeholder="Comment on Facebook" style="margin-bottom:-10px;width:100%;text-align:center;border-radius:none;padding:20px; margin-top:10px;font-size:14px;"></div>',
+    afterText: '<div style="border-top:1px solid #ccc;width:100%"><input id="commentbox" type="text" placeholder="Comment on Facebook" style="margin-bottom:-10px;width:100%;border:none;text-align:center;border-radius:none;padding:20px; margin-top:10px;font-size:14px;"></div>',
     buttons: [
       {
         text: '<i class="pe-7s-like2 pe-lg"></i>',
