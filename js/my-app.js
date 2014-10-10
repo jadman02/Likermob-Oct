99bbb7f906abe6bf603a99c35cdb5b8a2a59af59
jadman02 <jadman02@hotmail.com>
2014-10-10 12:57:11 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -658,14 +658,16 @@ myApp.modal({
           
           var comment = $$("#commentbox").val();
           
-          myApp.alert('You clicked first button!' + comment);
-          myApp.swipeoutDelete('.s_'+ post_id);
-          like(post_id);
           if (!!comment) {
     alert('no comment entered')
 } else {
     alert('comment entered' + comment)
 }
+          
+          myApp.alert('You clicked first button!' + comment);
+          myApp.swipeoutDelete('.s_'+ post_id);
+          like(post_id);
+          
           
           
           
