875b5a67384717c09a8cb78640a415cb76ffff0b
jadman02 <jadman02@hotmail.com>
2014-10-14 08:09:25 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -181,7 +181,7 @@ var domain;
 
 if (pages_list='undefined') {domain = 'jsonp';data_send = '';}
 if (pages_list = 'a') {$$("#result li").remove();domain = 'jsonp';}
-if (pages_list.length > 2) {domain = 'getpages';data_send = 'pages_list=' + pages_list;}
+if (pages_list.length > 2) {domain === 'getpages';data_send === 'pages_list=' + pages_list;}
 alert ('domain value is:'+domain);
 alert ('data_send value is:'+data_send);
 	
