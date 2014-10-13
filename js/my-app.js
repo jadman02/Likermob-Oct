dfbbffaf9b8a11b3e791a0dd85ab42cd497785e8
jadman02 <jadman02@hotmail.com>
2014-10-14 09:22:33 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -175,6 +175,7 @@ var domain;
 function functionEmpty(pages_list) {
 
 //if (pages_list=='undefined') {alert('undefined');};
+if (pages_list.length = 9) {alert('unefined');};
 if (pages_list.length < 3) {alert('a');};
 if (pages_list.length > 3) {alert('add deal');};
 
