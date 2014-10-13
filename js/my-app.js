fee8c199bdee0a9a4b338b65664fa96ba4742d61
jadman02 <jadman02@hotmail.com>
2014-10-13 23:14:41 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -176,7 +176,7 @@ function functionEmpty(pages_list) {
 //"pages_list=\'174238599281322\',\'50245567013\'"
 
 
-var data_send = 'pages_list=174238599281322,363053000439943';
+var data_send = 'pages_list="174238599281322","363053000439943"';
 var domain = 'getpages';
 
 alert(domain);
