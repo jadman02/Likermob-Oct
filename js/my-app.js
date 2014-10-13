56feab29dc9cbb0501f03d2c6fa2ba70dd67ce8a
jadman02 <jadman02@hotmail.com>
2014-10-14 09:27:20 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -175,9 +175,9 @@ var domain;
 function functionEmpty(pages_list) {
 
 //if (pages_list=='undefined') {alert('undefined');};
-if (pages_list.length = 9) {alert('unefined');};
+if (pages_list.length = 'undefined') {alert('unefined');};
 if (pages_list.length < 3) {alert('a');};
-if (pages_list.length > 3) {alert('add deal');};
+if (pages_list.length > 9) {alert('add deal');};
 
 
 //if (pages_list="undefined") {domain = "jsonp";data_send = "";};
