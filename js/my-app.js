bf341259748437b3e2edfa02118247e0f6dc738f
jadman02 <jadman02@hotmail.com>
2014-10-16 12:59:55 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -786,27 +786,6 @@ function dbDeal() {
         '  </div>' +
         '</div>'
     );
-    
-    openFB.api({
-        path: '/me/accounts',
-        success: function(response) {
-               
-                
-                
-                
-                
-                for (i = 0; i < response.data.length; i++) {
-                
-               $$( '#pages_list' ).append('<li>'+ response.data[i].name +'</li>');
-                
-                
-                }
-               
-                
-            },
-        error: function() {alert('error not posted on facebook');}
-        	
-        });
 	
 	
 }
