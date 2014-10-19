7373d173f3b0661aca66b9f4c12b4e7d2e4f2e70
jadman02 <jadman02@hotmail.com>
2014-10-19 22:09:58 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -32,7 +32,7 @@ for (i = 0; i < 10; i++)
 	
 
 	
-$$( '#resulta' ).append('<a href="#" onclick="saveAddress(\''+ response.predictions[i].place_id  +'\')"><li class="item-content">' + response.predictions[i].description + '</li></a>');
+$$( '#resulta' ).append('<a href="#" onclick="saveAddress(\''+ response.predictions[i].place_id  +'\')"><li class="item-content"><div class="item-inner">' + response.predictions[i].description + '</div></li></a>');
 }
 });
 });
