dad042fd424c3486b3473be0d54ee1f708c22adc
jadman02 <jadman02@hotmail.com>
2014-10-19 12:32:01 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -953,7 +953,21 @@ function addPhoto(){
 
 
 
-
+photoBrowser() {
+	
+	
+	navigator.camera.getPicture( function( imageURI ) {
+        alert( imageURI );
+      },
+      function( message ) {
+        alert( message );
+      },
+      {
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI
+      });
+	
+}
 
 
 
