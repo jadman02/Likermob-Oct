61215cf63eb50085dc501cf2db83c6e185158e8e
jadman02 <jadman02@hotmail.com>
2014-10-24 13:21:49 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1068,8 +1068,11 @@ email.value = "";
 opt.value =  "";
 opt.text = "";
 
+if (res[0][8] == "1") {alert('address is set');}
+else {alert('address is not set');}
+
 email.value = res[0][16];
-address.value = res[0][5] + ',' + res[0][6]  + ',' + res[0][7]  + ',' + res[0][9]  + ',' + res[0][10] ',' + res[0][11];
+//address.value = res[0][5] + ',' + res[0][6]  + ',' + res[0][7]  + ',' + res[0][9]  + ',' + res[0][10] ',' + res[0][11];
 phone.value = res[0][4];
 website.value = res[0][15];
 opt.value =  res[0][17];
