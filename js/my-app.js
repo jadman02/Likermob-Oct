2ba414ecce398cf8885bb759ff95c0c2a316d412
jadman02 <jadman02@hotmail.com>
2014-10-25 21:22:24 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1120,10 +1120,10 @@ $$('#add_button').remove();
 var img = new Image();
     img.src = $$( '.cover-add' ).css('background-image').replace(/url\(|\)$|"/ig, '');
     img.onload = function () {
-    image_width = img.width;
-    image_height = img.height;
+    var image_width = img.width;
+    var image_height = img.height;
     //windowsize = $$(window).width();
-    padding-top =  (image_height / image_width) * 320;
+    var padding-top =  (image_height / image_width) * 320;
     $$( '.content-block' ).css( 'padding-top', padding-top + 'px' );
 };
     
