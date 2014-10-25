d920c7ad60e7d22e5f6df16cc2a30582eb5de7b1
jadman02 <jadman02@hotmail.com>
2014-10-25 15:51:16 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1230,6 +1230,7 @@ function checkForm(id,min,max) {
 var input = document.getElementById(id + "_i").value;
 var date_now = new Date();
 alert(input);
+alert(date_now);
 var length_string = input.length;
 if (length_string < min) { errorForm(1);document.getElementById(id + "_c").innerHTML = '<a href="#" onclick="errorForm(1);" class="button" style="float:right;border:none;padding:0px;margin-top:10px;"><i class="pe-7s-attention pe-2x" style="color:#ff3b30;"></i></a>';}
 if (length_string > max) { errorForm(2);document.getElementById(id + "_c").innerHTML = '<a href="#" onclick="errorForm(2);" class="button" style="float:right;border:none;padding:0px;margin-top:10px;"><i class="pe-7s-attention pe-2x" style="color:#ff3b30;"></i></a>';}
