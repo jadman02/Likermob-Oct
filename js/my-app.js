f6251b2c3c6c31c8f612663c218cc81cbac1e2d5
jadman02 <jadman02@hotmail.com>
2014-10-26 22:46:55 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -444,7 +444,7 @@ nation.value = country;
 var elem = document.getElementById("fulladdress2");
 elem.innerHTML = 'Set location';
 
-if (subpremise) {document.getElementById("subpremise_i").value = subpremise;document.getElementById("subpremise_c").innerHTML = '<a href="#" class="button" style="float:right;border:none;padding:0px;border-radius:50%;margin-top:10px;"><i  class="pe-7s-check pe-2x"></i></a>';}
+if (subpremise !=='') {document.getElementById("subpremise_i").value = subpremise;document.getElementById("subpremise_c").innerHTML = '<a href="#" class="button" style="float:right;border:none;padding:0px;border-radius:50%;margin-top:10px;"><i  class="pe-7s-check pe-2x"></i></a>';}
 
 $$( '#fulladdress2' ).css( 'color', 'black' );
 document.getElementById("address_c").innerHTML = '<a href="#" class="button" style="float:right;border:none;padding:0px;border-radius:50%;margin-top:5px;"><i  class="pe-7s-check pe-2x"></i></a>';
