f6ba8ff0b88d235ac6ce46cfe15f240ab9ab3cf2
jadman02 <jadman02@hotmail.com>
2014-10-29 22:54:33 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -935,7 +935,7 @@ function dbDeal() {
                      
                       
  
-'<div class="tabbar">'+
+'<div class="toolbar tabbar">'+
     '<div class="toolbar-inner">'+
         '<a href="#tab1" onclick="clearPageslist();" class="tab-link active">'+
             '<i class="pe-7s-plus pe-lg"></i>'+
