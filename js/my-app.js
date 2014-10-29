67981f79d69e95ae7d814f7248802459371e1d53
jadman02 <jadman02@hotmail.com>
2014-10-29 23:27:02 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -938,16 +938,16 @@ function dbDeal() {
 '<div class="toolbar tabbar">'+
     '<div class="toolbar-inner">'+
         '<a href="#tab1" onclick="clearPageslist();" class="tab-link active">'+
-            '<i class="pe-7s-plus pe-2x icon"><span class="badge bg-red">1</span></i>'+
+            '<i class="pe-7s-plus pe-2x icon"><span class="badge bg-red">Pick</span></i>'+
         '</a>'+
         '<a href="#tab2" id="tab2link" class="tab-link disabled">'+
-           ' <i class="pe-7s-pen pe-2x icon"><span class="badge bg-red">2</span></i>'+
+           ' <i class="pe-7s-pen pe-2x icon"><span class="badge bg-red">Create</span></i>'+
        ' </a>'+
         '<a href="#tab3" id="tab3link" class="tab-link disabled">'+
-           ' <i class="pe-7s-switch pe-2x icon"><span class="badge bg-red">3</span></i>'+
+           ' <i class="pe-7s-switch pe-2x icon"><span class="badge bg-red">Customise</span></i>'+
        ' </a>'+
        ' <a href="#tab4" class="tab-link">'+
-            '<i class="pe-7s-upload pe-2x icon"><span class="badge bg-red">4</span></i>'+
+            '<i class="pe-7s-upload pe-2x icon"><span class="badge bg-red">Submit</span></i>'+
         '</a>'+
     '</div>'+
 '</div>'+
