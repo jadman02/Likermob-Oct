fcc06a94d7c98c9782c81cb35bea19bfaf2394e0
jadman02 <jadman02@hotmail.com>
2014-11-02 14:45:35 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1106,7 +1106,7 @@ document.getElementById("page_token").value = access_token;
 $$("#pages_list li").remove();	
 $$('#plusnavbar').show();
 $$('#toolbardeal').show();
-document.getElementById("toolbardeal").innerHTML = '<div class="toolbar tabbar" style="background-color:transparent;border:none;"><div class="toolbar-inner" style="background-color:transparent;"><a href="#" onclick="addPhoto('+page_id+')" class="tab-link" id="cameratab"><i class="pe-7s-camera pe-lg"></i></a><a href="#tab2" id="addresstab" class="tab-link" onclick="showAddress();document.getElementById(\'addressbox\').checked = true;"><i class="pe-7s-map-marker pe-lg"></i></a><a href="#tab2" id="clocktab" class="tab-link" onclick="toggleBox(\'check\');showDateTime();"><i class="pe-7s-clock pe-lg"></i></a><a href="#" id="commenttab" class="tab-link" onclick="togglebox(\'comment\');commentClick();"><i class="pe-7s-comment pe-lg"></i></a></div></div>';
+document.getElementById("toolbardeal").innerHTML = '<div class="toolbar tabbar" style="background-color:transparent;border:none;"><div class="toolbar-inner" style="background-color:transparent;"><a href="#" onclick="addPhoto('+page_id+')" class="tab-link" id="cameratab"><i class="pe-7s-camera pe-lg"></i></a><a href="#tab2" id="addresstab" class="tab-link" onclick="document.getElementById(\'addressbox\').checked = false;showAddress();document.getElementById(\'addressbox\').checked = true;"><i class="pe-7s-map-marker pe-lg"></i></a><a href="#tab2" id="clocktab" class="tab-link" onclick="toggleBox(\'check\');showDateTime();"><i class="pe-7s-clock pe-lg"></i></a><a href="#" id="commenttab" class="tab-link" onclick="togglebox(\'comment\');commentClick();"><i class="pe-7s-comment pe-lg"></i></a></div></div>';
 
 
 
