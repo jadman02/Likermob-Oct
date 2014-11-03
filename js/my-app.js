5c4e683d3127fbcdfbbd85d82e49ad23206f42aa
jadman02 <jadman02@hotmail.com>
2014-11-03 22:13:07 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -212,8 +212,7 @@ var str = response[i][4];
 var singlequote = str.replace(/'/g, "qqqq");
 //var description = "'" + singlequote.replace(/(\r\n|\n|\r)/gm,"") + "'";
 var type = response[i][9];
-
-alert(response[i][21]);
+if (response[i][5]) {alert(response[i][5]));}
 
 if (response[i][21] !== '0'){photo_created = response[i][21];alert(photo_created);}
 //if (response[i][21]) {cover = 'http://smilesavers.net.au/images/likermob/' + response[i][1] + '_' + response[i][21] + '.jpg';}
