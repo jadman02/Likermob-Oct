2d02c1e2691d4744943a887b7407263584651b1b
jadman02 <jadman02@hotmail.com>
2014-11-04 19:18:55 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -204,7 +204,7 @@ $$.getJSON('http://www.smilesavers.net.au/'+ domain +'.php?callback=?', ''+ data
 localStorage.setItem("total_pages", response.length);
 $$("#loader-container").hide();
 
-if (pages_list=='a' || typeof pages_list === 'undefined') {$$( '#result' ).append('<li><div class="buttons-row"><a href="#tab1" class="tab-link active button">New</a><a href="#tab2" class="tab-link button">Nearby</a><a href="#tab3" class="tab-link button">Expiring</a></div></li>');}
+
 
    
 for (i = 0; i < 10; i++) {        
