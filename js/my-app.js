135e47fe3a96bc9836114a52a6a895c3891bfe60
jadman02 <jadman02@hotmail.com>
2014-11-04 23:50:48 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1348,9 +1348,9 @@ function openSearchHome() {
     '<div class="center">'+
 
  '<div class="buttons-row" style="padding:5px;">'+
-       ' <a href="#tab1" class="tab-link active button" style="padding-left:10px;padding-right:10px;"><i class="pe-7s-map-marker pe-lg"></i></a>'+
-        '<a href="#tab2" class="tab-link button" style="padding-left:10px;padding-right:10px;"><i class="pe-7s-photo-gallery pe-lg"></i></a>'+
-        '<a href="#tab3" class="tab-link button" style="padding-left:10px;padding-right:10px;"><i class="pe-7s-filter pe-lg"></i></a>'+
+       ' <a href="#tab1" class="tab-link active button"><i class="pe-7s-map-marker pe-lg" style="padding-left:10px;padding-right:10px;"></i></a>'+
+        '<a href="#tab2" class="tab-link button"><i class="pe-7s-photo-gallery pe-lg" style="padding-left:10px;padding-right:10px;"></i></a>'+
+        '<a href="#tab3" class="tab-link button"><i class="pe-7s-filter pe-lg" style="padding-left:10px;padding-right:10px;"></i></a>'+
       '</div>'+
     '</div>'+
  ' </div>'+
