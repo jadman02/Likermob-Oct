91eabb94b668a5b2ec23e2c7331e0a4c65626228
jadman02 <jadman02@hotmail.com>
2014-11-07 10:19:27 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1334,6 +1334,7 @@ function openSearch() {
 function openSearchHome() {
 
   var heightslider = $$(window).height() - 64;
+  var heightrange = $$(window).height() - 90;
 
 $$("#result li").remove();
 $$( ".business" ).remove();
@@ -1371,7 +1372,7 @@ var position = localStorage.getItem("position");
 
 
 '<div class="item-input rangeslider" style="-webkit-transform: rotate(-90deg);float:left;width:20%;">'+
-'<div class="range-slider" id="radius" name="radius" style="width:'+ heightslider +'px;margin-left:-300px;">'+
+'<div class="range-slider" id="radius" name="radius" style="width:'+ heightrange +'px;margin-left:-300px;">'+
 '<input type="range" id="rangeInput" name="rangeInput" min="20" max="99" value="25" oninput="amount.value=rangeInput.value">'+                                                       
 '</div>'+
 '</div>'+
