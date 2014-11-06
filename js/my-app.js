1fb15f0b65786624c37c7e354825c9c0574507e2
jadman02 <jadman02@hotmail.com>
2014-11-06 19:49:44 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1342,7 +1342,7 @@ function openSearch() {
 
 function openSearchHome() {
 
-var heightslider = $$(window).height() - 44 + 'px';
+var heightslider = $$(window).height() - 100;
 
 $$("#result li").remove();
 $$( ".business" ).remove();
@@ -1362,7 +1362,7 @@ var position = localStorage.getItem("position");
  ' </div>'+
 '</div>'+
 
-'<div class="slider-custom" style="height: '+ heightslider +'"><div class="slider-container slider-init"><div class="slider-pagination"></div><div class="slider-wrapper">'+
+'<div class="slider-custom" style="height: '+ heightslider +' px"><div class="slider-container slider-init"><div class="slider-pagination"></div><div class="slider-wrapper">'+
         '<div class="slider-slide">'+
         
                      ' <form class="searchbar" style="background-color:#ff8000; border:0;">'+
