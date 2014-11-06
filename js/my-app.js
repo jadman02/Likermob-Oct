84ee26c66ef1c6e6f97906812d807dd50d8bca4e
jadman02 <jadman02@hotmail.com>
2014-11-07 00:20:42 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1371,8 +1371,8 @@ var position = localStorage.getItem("position");
 
 
 '<div class="item-input rangeslider" style="-webkit-transform: rotate(-90deg);">'+
-'<div class="range-slider" id="radius" name="radius" style="margin-top:60px;">'+
-'<input type="range" id="rangeInput" name="rangeInput" min="20" max="99" value="25" oninput="amount.value=rangeInput.value">'+                                                       
+'<div class="range-slider" id="radius" name="radius">'+
+'<input type="range" id="rangeInput" name="rangeInput" style="margin-top:60px;" min="20" max="99" value="25" oninput="amount.value=rangeInput.value">'+                                                       
 '</div>'+
 '</div>'+
 
