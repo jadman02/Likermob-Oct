358250024e9bb04a0c37d371e5048dded503cd05
jadman02 <jadman02@hotmail.com>
2014-11-07 10:55:02 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1357,13 +1357,13 @@ var position = localStorage.getItem("position");
 '<div class="slider-custom"><div class="slider-container slider-init"><div class="slider-wrapper">'+
         '<div class="slider-slide">'+
         
-'<div class="list-block" style="float:left;width:80%;"><ul><li><div class="item-content"><div class="item-inner"><div class="item-input" style=""><input type="text" placeholder="'+ position +'" onkeyup="searchPlaces(\'set\');" id="fulladdressf"></div></div></div></ul></li></div>'+
-
-          
-   
-'<div class="item-title label" style="background-color:white;border-radius:5px;margin-left:5px;padding:4px;float:left;width:20%;"><output name="amount" id="amount" for="rangeInput">20</output> km</div>'+
+                     ' <form class="searchbar" style="background-color:#ff8000; border:0;">'+
+       ' <div class="searchbar-input">'+
+          '  <input type="search" placeholder="'+ position +'" onkeyup="searchPlaces(\'set\');" id="fulladdressf">'+
+       ' <a href="#" class="searchbar-clear"></a></div>'+
+'<div class="item-title label" style="background-color:white;border-radius:5px;margin-left:5px;padding:4px;float:left;width:60px;"><output name="amount" id="amount" for="rangeInput">20</output> km</div>'+
 //'<a href="#" class="button" style="margin-left:5px;color:white;border:0;"><i class="pe-7s-signal pe-lg"></i></a>'+
-
+   ' </form>'+
 
 
 '<div class="content-block">'+
