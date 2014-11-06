cd844047019f07d64298b973f43682a29667aecc
jadman02 <jadman02@hotmail.com>
2014-11-06 13:47:12 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1364,7 +1364,7 @@ function openSearchHome() {
        ' <div class="searchbar-input">'+
           '  <input type="search" placeholder="Search for address" onkeyup="searchPlaces();" id="fulladdress">'+
        ' </div>'+
-'<a href="#" class="button" style="margin-left:5px;color:white;"><i class="pe-7s-signal pe-lg"></i></a>'+
+'<a href="#" class="button" style="margin-left:5px;color:white;border:0;"><i class="pe-7s-signal pe-lg"></i></a>'+
    ' </form>'+
 
 
