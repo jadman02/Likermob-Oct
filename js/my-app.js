6e037576d2340a98e145ff60daaa3281349463ca
jadman02 <jadman02@hotmail.com>
2014-11-09 11:18:53 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -13,40 +13,7 @@ document.getElementById("profilepic").innerHTML = '<img src="http://graph.facebo
 
     functionEmpty();
 
-window.storekit.init({
 
-    debug: true, /* Because we like to see logs on the console */
-
-    purchase: function (transactionId, productId) {
-        alert('purchased: ' + productId);
-    },
-    restore: function (transactionId, productId) {
-        alert('restored: ' + productId);
-    },
-    restoreCompleted: function () {
-       alert('all restore complete');
-    },
-    restoreFailed: function (errCode) {
-        alert('restore failed: ' + errCode);
-    },
-    error: function (errno, errtext) {
-        alert('Failed: ' + errtext);
-    },
-    ready: function () {
-        var productIds = [
-            "com.likermob.test.qwerty1234", 
-            "com.likermob.test.qwerty12345"
-        ];
-        window.storekit.load(productIds, function(validProducts, invalidProductIds) {
-            $.each(validProducts, function (i, val) {
-                alert("id: " + val.id + " title: " + val.title + " val: " + val.description + " price: " + val.price);
-            });
-            if(invalidProductIds.length) {
-                alert("Invalid Product IDs: " + JSON.stringify(invalidProductIds));
-            }
-        });
-    }
-});
 
 
 
