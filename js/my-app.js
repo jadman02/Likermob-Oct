46e465da5d1e43733a27443f6a1409b696de5f73
jadman02 <jadman02@hotmail.com>
2014-11-09 23:24:59 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -835,7 +835,7 @@ $$.getJSON('https://graph.facebook.com/'+ page_id +'?fields=cover', function(res
         '    <div class="page-content cover-business" style="z-index: 1;margin-top:30px;">' +
         '      <div class="content-block" style="padding-top:40px;">' +
         '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);"">' +
-        '<p class="buttons-row theme-orange" style="margin-top:-50px;"><a href="#" class="button active" onclick="favLis('+ page_id +');"><i class="pe-7s-star pe-2x"></i></a><a href="#" class="button active"><i class="pe-7s-call pe-2x"></i></a><a href="#" class="button active"><i class="pe-7s-share pe-2x"></i></a></p>' + 
+        '<p class="buttons-row theme-orange" style="margin-top:-50px;"><a href="#" class="button active" onclick="favList('+ page_id +');"><i class="pe-7s-star pe-2x"></i></a><a href="#" class="button active"><i class="pe-7s-call pe-2x"></i></a><a href="#" class="button active"><i class="pe-7s-share pe-2x"></i></a></p>' + 
         '<div class="content-block-title">Contact</div><div class="list-block media-list"><ul>' + 
         '<li><a href="#" class="item-content"><div class="item-media"><img src="http://graph.facebook.com/'+ page_id +'/picture?width=50&height=50" style="border-radius:50%;margin-right:10px;"/></div><div class="item-inner"><div class="item-title-row"><div class="item-title">'+ res[0][1] +'</div></div><div class="item-subtitle">'+ res[0][6] + ' ' + res[0][7] + ' ' +  res[0][8] + '</div><div class="item-text">'+ res[0][9] + ' ' + res[0][10] + ' ' + res[0][11]+'</div></div></a></li>' +
         '</ul></div>' +
